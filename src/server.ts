7a31c1ccefb68bca79cceb15ab88ab33869c346c
nehaltariq357 <143392985+nehaltariq357@users.noreply.github.com>
2026-09-16 18:53:12 +0500

    update

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ import app from './app.js';
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}/api`);
 }   )
